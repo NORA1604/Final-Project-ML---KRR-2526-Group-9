641614119992e6de941c29ba752a42770ba0964c
Nigel Oliver Agojo <168045975+NORA1604@users.noreply.github.com>
2026-01-06 10:03:43 +0800

    Delete docs directory

--- docs/krr_engine.py
+++
@@ -1,9 +0,0 @@
-def restock_rules(usage_level, avg_usage):
-    if usage_level == "HIGH" and avg_usage > 5:
-        return "RESTOCK IMMEDIATELY"
-    elif usage_level == "HIGH":
-        return "RESTOCK SOON"
-    elif avg_usage > 3:
-        return "MONITOR STOCK"
-    else:
-        return "STOCK OK"
